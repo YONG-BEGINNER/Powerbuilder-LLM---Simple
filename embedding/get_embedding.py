832966c2e45782e33056e2d14182d99fe6aa99d8
YONG-BEGINNER <yym013@gmail.com>
2025-05-30 11:36:51 +0800

    50% Progress on development LLM with interfaced

---
+++ embedding/get_embedding.py
@@ -0,0 +1,11 @@
+# Require Pacakge
+# pip install rapidocr_onnxruntime 
+# pip install "unstructured[all-docs]" 
+# pip install pyMuPDF
+
+# To apply Embedding
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def get_embedding(embedding: str, embedding_key:str=None, env_file:str = None):
+   if embedding == "m3e":
+      return HuggingFaceEmbeddings(model = "moka-ai/m3e-base")#, model_kwargs = {'device': device})
